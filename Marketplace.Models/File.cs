7d28492e95eb33c3e65cf1f18ff33e91ae67f3d6
sasholilov <alex_xld@abv.bg>
2017-04-17 17:58:05 +0300

    AddFile

---
+++ Marketplace.Models/File.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Models
+{
+
+    public class File
+    {
+
+        public File()
+        {
+            this.Products = new HashSet<Product>();
+            this.Category = new HashSet<Category>();
+        }
+
+        public int Id { get; set; }
+
+
+        [StringLength(255)]
+        public string FileName { get; set; }
+
+        [StringLength(100)]
+        public string ContentType { get; set; }
+
+
+        public byte[] Content { get; set; }
+
+
+        public FileType FileType { get; set; }
+
+
+        public virtual ICollection<Product> Products { get; set; }
+
+
+        public virtual ICollection<Category> Category { get; set; }
+    }
+    public enum FileType
+    {
+        Avatar = 1, Photo
+    }
+}
